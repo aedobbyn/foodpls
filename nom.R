b3ec9629c5221717d1a410234c0ee9c4104ca26e
aedobbyn <adobbs11@gmail.com>
2020-04-12 16:40:32 -0400

    don't run hit_window if we're not on the right page

--- nom.R
+++ nom.R
@@ -17,103 +17,8 @@ place_order_name <- "placeYourOrder1"
 available_windows_text <- "Next available"
 success_text <- "Thank you, your Whole Foods Market order has been placed."
 
-browse.launch_session <- function(
-  url = "https://democrats.org",
-  port = 4444L,
-  server_name = ".deck_headless",
-  server_environment = .GlobalEnv,
-  browser = "chrome",
-  download_folder = NA, # Only applicable for Chrome
-  use_version = "lowest",
-  ...) {
-  browse.close_session(server_environment = server_environment)
-  
-  # Make sure the port is valid
-  if (port == 4444L) {
-    while (any(!is.na(pingr::ping_port("localhost", port)))) {
-      port %<>% add(1) %>% as.integer()
-    }
-  }
-  
-  # Create simulated display if needed
-  if (!sys.is_osx() & browser == "chrome") {
-    display_pids <- sys.cmd("pgrep -f vfb") %>% .[. %in% sys.cmd("pgrep -f vfb")]
-    if (length(display_pids) >= 1) for (pid in display_pids) sys.cmd(paste("kill -9", pid))
-    system("Xvfb :99 -ac", wait = FALSE)
-    Sys.setenv("DISPLAY" = ":99")
-  }
-  
-  # Start a PhantomJS driver accessible from the global environment
-  assign(
-    server_name,
-    if (browser == "phantomjs") {
-      wdman::phantomjs(port = as.integer(port), ...)
-    } else
-      if (browser == "chrome") {
-        wdman:::chrome_check(verbose = T)
-        
-        os <-
-          ifelse(
-            Sys.info()[["sysname"]] == "Darwin",
-            "mac64", "linux64"
-          )
-        
-        versions <- binman::list_versions("chromedriver")[[os]]
-        
-        if (use_version == "lowest") {
-          v_idx <- 1
-        } else if (use_version == "highest") {
-          v_idx <- length(versions)
-        } else if (is.numeric(use_version)) {
-          v_idx <- use_version
-        } else {
-          stop("use_version must be numeric or one of: 'lowest', 'highest'.")
-        }
-        
-        wdman::chrome(
-          port = as.integer(port),
-          version = versions[v_idx],
-          ...
-        )
-      } else {
-        stop("Invalid browser.")
-      },
-    envir = server_environment
-  )
-  
-  # Browse to the given URL on the PhantomJS driver
-  site <-
-    seleniumPipes::remoteDr(
-      browserName = browser,
-      port = as.integer(port),
-      extraCapabilities = list(
-        chromeOptions = list(
-          args = list("--no-sandbox"),
-          prefs = list(
-            "profile.default_content_settings.popups" = 0L,
-            "download.prompt_for_download" = FALSE,
-            "download.default_directory" = if (is.na(download_folder)) getwd() else download_folder
-          )
-        )
-      )
-    ) %>%
-    seleniumPipes::go(url) %>% 
-    seleniumPipes::addCookie(name = "session-id", value = "135-9942881-1066911") %>% 
-    seleniumPipes::addCookie(name = "session-time", value = "2082787201l") %>% 
-    seleniumPipes::addCookie(name = "session-token", value = "bOjytyUSh5D99jpWDknCwlWgDkZx8coOGn/dK2cGpmbAgg7Dki5kIrjOIlCPqe4py3rVKqDjCrJ5TNmNZuaex063xkwRJLd9w4ZPncMNTyxoiwligCV0CuLaFM3/01AHbTbExwh1AOa0ROEaan+z4vg9x74/Bosxy345OPEboJer0cdgxUrfR0q3mns0JmBe2y7mEjnMqBfgDgexg7Re9ldGqWTEdsrkQwrCFV6s8522wuLmvHNZz2YhHc/3kiqKo2Sr6xQxSg1BTV1HTEY7jg==") %>% 
-    seleniumPipes::addCookie(name = "ubid-main", value = "132-9234009-4556454")
-  return(site)
-}
-
 browse.start_session <- function(url, browser = "chrome", port = 4444L) {
   browse.close_session()
-
-  # Make sure the port is valid
-  if (port == 4444L) {
-    while (any(!is.na(pingr::ping_port("localhost", port)))) {
-      port %<>% add(1) %>% as.integer()
-    }
-  }
 
   # Create simulated display if needed
   if (!sys.is_osx() & browser == "chrome") {
@@ -133,18 +38,26 @@ browse.start_session <- function(url, browser = "chrome", port = 4444L) {
 }
 
 auth <- function(sess) {
+  message("Inputting password.")
   # Input password
   sess %>% 
     browse.send_text("id", password_id, pass)
   
+  Sys.sleep(1)
+  
+  message("Hitting continue.")
   sess %>% 
     browse.click_element("id", sign_in_id)
 }
 
 sess <- browse.start_session(url)
 
+Sys.sleep(1)
+
 sess %>% 
   browse.send_text("id", email_id, "amanda.e.dobbyn@gmail.com")
+
+Sys.sleep(1)
 
 sess %>% 
   browse.click_element("id", "continue")
@@ -153,25 +66,47 @@ Sys.sleep(2)
 
 auth(sess)
 
-# Assume we can get past 2FA
+Sys.sleep(1)
 
 sess %>% 
   browse.click_element("id", cart_id)
 
-Sys.sleep(2)
+Sys.sleep(1)
 
 sess %>% 
   browse.click_element("name", checkout_name)
+
+Sys.sleep(2)
 
 # Skip the "before you checkout do you want to add stuff"
 sess %>% 
   browse.click_element("name", proceed_to_checkout_name)
 
+Sys.sleep(1)
+
 # Skip the note about subsitutions
 sess %>% 
   browse.click_element("class", continue_class)
 
+Sys.sleep(1)
+
+check_on_schedule_order_page <- function(sess) {
+  on_correct_page <- 
+    sess %>% 
+      browse.extract_html() %>% 
+      rvest::html_nodes("h1") %>% 
+      rvest::html_text() %>% 
+      stringr::str_detect("Schedule your order") %>% 
+      any()
+  
+  if (!on_correct_page) message("Didn't make it to Schedule Order page.")
+  
+  on_correct_page
+}
+
 hit_window <- function(n_tries = 100, timeout_after = 30*60, and = TRUE) {
+  if (!check_on_schedule_order_page(sess)) return(invisible())
+  
   `%*%` <- if (and) `&&` else `||`
   
   start_time <- lubridate::now()
@@ -237,6 +172,8 @@ hit_window <- function(n_tries = 100, timeout_after = 30*60, and = TRUE) {
         return(invisible())
       } 
     } 
+    Sys.sleep(runif(1, 0.5, 1.5))
+    
     seleniumPipes::refresh(sess)
     try_num %<>% +1
     elapsed_time <- (lubridate::now() - start_time) %>% round(0)
