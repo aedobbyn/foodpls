1c36b4a3e5041d89b7db26124cddc9a7c0ef4952
aedobbyn <adobbs11@gmail.com>
2020-04-13 08:55:35 -0400

    add readme

---
+++ data-raw/cookies.R
@@ -0,0 +1,13 @@
+
+cookies_template <- 
+  tibble::tibble(
+    name = c(
+      "session_id",
+      "session-time",
+      "session-token",
+      "ubid-main"
+    ),
+    value = rep("", 4)
+  )
+
+readr::write_csv(cookies_template, "data-raw/cookies_template.csv")
